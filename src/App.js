8f61a69422d7047cfe35038c815918a13e36304d
imcodingdreams <naykavallejo@yahoo.com>
2023-02-26 14:18:26 -0800

    Populating Homeworld and Species in the table

--- src/App.js
+++ src/App.js
@@ -21,10 +21,27 @@ function App() {
     };
     fetchCharacterData();
   }, []);
-debugger
+
+  useEffect(() => {
+    async function fetchAdditionalInfo() {
+      const newData = await Promise.all(
+        characterInfo.map(async (character) => {
+          const homeworldInfo = await axios.get(`https://swapi.dev/api/planets/1/`);
+          const speciesInfo = await axios.get(`https://swapi.dev/api/species/2/`);
+          return {
+            ...character,
+            homeworld: homeworldInfo.data.name,
+            species: speciesInfo.data.name
+          };
+        })
+      );
+      setCharacterInfo(newData);
+    }
+    fetchAdditionalInfo();
+  }, [characterInfo]);
+
   const filteredData = characterInfo.filter(character =>
-    character.name.toLowerCase().includes(filterValue.toLowerCase())
-  );
+    character.name.toLowerCase().includes(filterValue.toLowerCase()));
 
   return (
     <div>
@@ -37,129 +54,3 @@ debugger
 
 
 export default App;
-
-// function App() {
-//   const [characterInfo, setCharacterInfo] = useState('')
-//   const [characterSpecies, setCharacterSpecies] = useState('')
-//   const getCharacterInfo = () => {
-//     axios.get('https://swapi.dev/api/people', 'https://swapi.dev/api/species')
-//       .then(response => {
-//         let character = response.data.results;
-
-//         let characterName = character[1].name;
-//         let birthdate = character[1].birth_year;
-//         let height = character[1].height;
-//         let mass = character[1].mass;
-
-//         let characterSpecies = response.data.results;
-//         let species = characterSpecies[1].species;
-
-//         setCharacterInfo({ characterName, birthdate, height, mass })
-//         setCharacterSpecies({ species })
-//         console.log(characterName, birthdate, height, mass, species)
-
-//       }).catch(err => {
-//         console.log(err)
-//       });
-//   };
-
-//   console.log()
-//   return (
-//     <div>
-//       <Header />
-//       {/* <LiveSearchFilter /> */}
-//       <Form getCharacterInfo={getCharacterInfo} />
-//       <CharactersTable characterInfo={characterInfo} characterSpecies={characterSpecies} />
-//     </div>
-//   );
-// }
-
-//const characterData = 'https://swapi.dev/api/people?search';
-//const characterSpeciesData = 'https://swapi.dev/api/species';
-
-// function App () {
-//   const characterData = 'https://swapi.dev/api/people?search=';
-//   const [characterInfo, setCharacterInfo] = useState('');
-
-//   useEffect(() => {
-//     async function getCharacterInfo() {
-//       let response = await axios.get(characterData);
-//       let character = response.data.results;
-
-//       let characterName = character.name;
-//       let birthdate = character.birth_year;
-//       let height = character.height;
-//       let mass = character.mass;
-//       let species = character.species;
-
-//       setCharacterInfo({ characterName, birthdate, height, mass, species})
-//       //setCharacterInfo(response.data.results)
-//       console.log(characterInfo)
-//     }
-//   }, []);
-//     return (
-//     <div>
-//       <Header />
-//       {/* <LiveSearchFilter /> */}
-//       <Form getCharacterInfo={getCharacterInfo}/>
-//       <CharactersTable characterInfo={characterInfo} />
-//     </div>
-//   );
-//   }
-
-//https://swapi.dev/api/people?search=Darth
-
-// const characterData = 'https://swapi.dev/api/people';
-// const characterSpeciesData = 'https://swapi.dev/api/species'
-
-// const App = () => {
-//   const [characterInfo, setCharacterInfo] = useState([])
-//   const [characterSpecies, setCharacterSpecies] = useState([])
-
-//   useEffect(() => {
-//     async function getCharacterInfo() {
-//       const response = await axios.get(characterData, characterSpeciesData);
-//       console.log(response)
-//     }
-//   })
-//     return (
-//     <div>
-      
-//     </div>
-//   );
-//   }
-
-//   const getCharacterInfo = () => {
-    
-//       .then(response => {
-//         let character = response.data.results;
-
-//         let characterName = character[1].name;
-//         let birthdate = character[1].birth_year;
-//         let height = character[1].height;
-//         let mass = character[1].mass;
-
-//         let characterSpecies = response.data.results;
-//         let species = characterSpecies[1].species;
-
-//         setCharacterInfo({ characterName, birthdate, height, mass })
-//         setCharacterSpecies({ species })
-//         console.log(characterName, birthdate, height, mass, species)
-
-//       }).catch(err => {
-//         console.log(err)
-//       });
-//   };
-
-//   console.log(response)
-//   return (
-//     <div>
-//       <Header />
-//       {/* <LiveSearchFilter /> */}
-//       <Form getCharacterInfo={getCharacterInfo} />
-//       <CharactersTable characterInfo={characterInfo} characterSpecies={characterSpecies} />
-//     </div>
-//   );
-// }
-
-
